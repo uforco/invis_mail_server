5f2a79b92776ede606418e44044b1c4cc08d6605
Md Sharif <srka780@gmail.com>
2024-01-31 05:44:14 +0600

    maile server

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const cliendss = require("./cliendsms/cliendss");
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5174", "http://192.168.31.254:5173", ""],
+  })
+);
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "uforcode123@gmail.com",
+    pass: "wynd ejlr dqlx wyni",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/email", async (req, res) => {
+  console.log(req.body);
+  const infomaile = req.body
+
+  const mailOnwr = {
+    from: 'ufore@gmail.com',
+    to: ['srka780@gmail.com'],
+    subject: "invis Cliend Email", // Subject line
+    html: `
+    <p><strong>Company :</strong> &nbsp;${infomaile?.company}</p>
+
+    <p><strong>Client Name :</strong>&nbsp; ${infomaile?.name}</p>
+    
+    <p><strong>Client contact :</strong>&nbsp; ${infomaile?.number}</p>
+    
+    <p><strong>Client Email :</strong> ${infomaile?.gmail}</p>
+    
+    <p><strong>Project Detail&nbsp;-</strong></p>
+    
+    <p>${infomaile?.project_detail}</p>
+    `, // plain text body
+  }
+
+  const mailOptions = {
+    from: 'ufore@gmail.com',
+    to: [`${infomaile?.gmail}`],
+    subject: "Thank you for the Email ✔", // Subject line
+    html: cliendss, // html body
+  }
+  
+  // Send the email
+  transporter.sendMail(mailOnwr, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.send({mail: "mail server Error"});
+    } else {
+      console.log('Email sent: ' + info.response);
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error(error);
+          res.send({mail: "mail server Error"});
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.send({mail: "successfully"});
+        }
+      });
+    }
+  });
+
+
+  // res.send(info.messageId);
+
+
+
+
+
+
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
